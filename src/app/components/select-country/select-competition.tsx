63a3868d175cb7bf7cfebbc2448b06d95c0156f5
Sergey Svezhenets <ssvezhenets@tradingview.com>
2025-01-16 17:35:44 +0300

    Add competition selection

--- src/app/components/select-country/select-competition.tsx
+++ src/app/components/select-country/select-competition.tsx
@@ -7,16 +7,19 @@ export default function SelectCompetition() {
 			sx={{ padding: '1rem' }}
 		>
 			<FormControl fullWidth>
-				<InputLabel id="competition-select-label" className={styles.selectLabel}>Competition</InputLabel>
+				<InputLabel
+					id="competition-select-label"
+					className={styles.selectLabel}
+				>Competition
+				</InputLabel>
 				<Select
 					labelId="competition-select-label"
 					id="competition-select"
 					className={styles.select}
-					value={'E0'}
 				>
-					<MenuItem value="E0" className={styles.selectItem}>England Premier League</MenuItem>
-					<MenuItem value="D1" className={styles.selectItem}>Germany Bundesliga</MenuItem>
-					<MenuItem value="ES1" className={styles.selectItem}>Spain LaLiga</MenuItem>
+					<MenuItem id="E0" value="E0" className={styles.selectItem}>England Premier League</MenuItem>
+					<MenuItem id="D1" value="D1" className={styles.selectItem}>Germany Bundesliga</MenuItem>
+					<MenuItem id="ES1" value="ES1" className={styles.selectItem}>Spain LaLiga</MenuItem>
 				</Select>
 			</FormControl>
 		</Box>
