79a96710d30762c8462b1d1bf72ea420a3cfcd12
Sergey Svezhenets <ssvezhenets@tradingview.com>
2025-01-17 13:11:33 +0300

    Fix css colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import Header from '@/app/components/header/header';
 import MainSelect from '@/app/components/mainSelect/mainSelect';
 import { selectCompetitionProps } from '@/app/constants/select-competition-props';
 
-import styles from "./page.module.css";
-
 export default function Home() {
 
 	return (
-		<div className={styles.page}>
+		<div>
             <Header></Header>
 			<MainSelect items={selectCompetitionProps.items}></MainSelect>
 		</div>
