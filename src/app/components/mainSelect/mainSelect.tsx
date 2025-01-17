cb580aeabcdedce18a165188148fae1267f91cfd
Sergey Svezhenets <ssvezhenets@tradingview.com>
2025-01-17 16:57:20 +0300

    Fix css colors

--- src/app/components/mainSelect/mainSelect.tsx
+++ src/app/components/mainSelect/mainSelect.tsx
@@ -18,18 +18,18 @@ export default function MainSelect(props: SelectProps) {
 
 	return (
 		<div
-			className={`${styles.selectContainer}`}
+			className={styles.selectContainer}
 		>
 			<FormControl fullWidth>
 				<InputLabel
 					id="competition-select-label"
-					className={`${styles.selectLabel}`}
+					className={styles.selectLabel}
 				>Competition
 				</InputLabel>
 				<Select
 					labelId="competition-select-label"
 					id="competition-select"
-					className={`${styles.select}`}
+					className={styles.select}
 				>
 					{menuItems}
 				</Select>
